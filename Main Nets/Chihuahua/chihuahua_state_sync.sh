d28ccade07d5f86141b2df8e50c6635304c40e06
TrustTech <161213942+Dr0ff@users.noreply.github.com>
2025-07-11 16:15:13 +0100

    Update chihuahua_state_sync.sh

--- Main Nets/Chihuahua/chihuahua_state_sync.sh
+++ Main Nets/Chihuahua/chihuahua_state_sync.sh
@@ -16,5 +16,5 @@ s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ;
 s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ;
 s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ;
 s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.chihuahuad/config/config.toml
-sudo systemctl start chihuahuad.service
+sudo systemctl restart chihuahuad.service
 sudo journalctl -u chihuahuad -f --output cat
